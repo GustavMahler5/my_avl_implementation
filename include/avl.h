d7d1ea892f0f5df279552b63dd7123779ce97d27
Jason Holtman <87673269+GustavMahler5@users.noreply.github.com>
2026-04-10 11:42:59 -0700

    Created avl.h

---
+++ include/avl.h
@@ -0,0 +1,31 @@
+#ifndef AVL_H
+#define AVL_H
+
+#include <string>
+
+using namespace std;
+
+class myTree {
+private:
+    struct Node {
+        string data;
+        Node* left;
+        Node* right;
+        int height;
+        int subtreeSize;
+
+        Node(string value);
+    };
+
+    Node* root;
+    int getHeight(Node* node);
+    int getBalance(Node* node);
+    Node* rotateLeft(Node* x);
+    Node* rotateRight(Node* y);
+    Node* insert(Node* node, string value);
+
+public:
+    AVLTree();
+    void insert(string value);
+};
+#endif
